f0c42e4176848168a3217cd8d5c4b62ee7259c02
a464442700 <464442700@qq.com>
2023-06-15 18:20:49 +0800

    2023-06-15-01

---
+++ Migration/src/main/java/com/lxf/migration/pojo/DataSourceBox.java
@@ -0,0 +1,12 @@
+package com.lxf.migration.pojo;
+
+public class DataSourceBox {
+   public String  name;//数据库实例名
+   public String  value;//数据源名称
+
+
+   public DataSourceBox(String name, String value) {
+      this.name = name;
+      this.value = value;
+   }
+}
